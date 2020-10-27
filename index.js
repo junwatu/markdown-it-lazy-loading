b6b5a328e00b169d9755da2799d3716b8057561c
Equan Pr <equan.p@gmail.com>
2020-10-27 10:36:55 +0700

    basic code

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let lzAttr = {};
+const toArray = (arr) => (Array.isArray(arr) ? arr : [arr]);
+
+function parseTokens(tokens) {
+  tokens.forEach((token) => {
+    if (/(_open$|image)/.test(token.type) && lzAttr[token.tag]) {
+      const addition = toArray(lzAttr[token.tag]);
+      token.attrSet("loading", [...addition].join(" "));
+    }
+    if (token.children) {
+      parseTokens(token.children);
+    }
+  });
+}
+
+function parseState(state) {
+  parseTokens(state.tokens);
+}
+
+function markdownItLazyLoading(md, _lzAttr) {
+  lzAttr = _lzAttr || {};
+  md.core.ruler.push("markdown-it-lazy-loading", parseState);
+}
+
+module.exports = markdownItLazyLoading;
