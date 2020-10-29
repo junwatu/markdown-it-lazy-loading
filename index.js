3615047dc6c411cf8ec56017e2842e229d91912d
Equan Pr <equan.p@gmail.com>
2020-10-29 18:44:59 +0700

    create noscript tag for loading polyfill

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 "use strict";
 
-let lzAttr = {};
+const Token = require("markdown-it/lib/token");
 const toArray = (arr) => (Array.isArray(arr) ? arr : [arr]);
 
+let _tmp = [];
+let lzAttr = {};
+/**
+ * loading lazy polyfill noscript
+ */
+
+let _nsOpenToken = new Token("noscript_open", "noscript", 0);
+_nsOpenToken.attrSet("class", "loading-lazy");
+let _nsCloseToken = new Token("noscript_close", "noscript", -1);
+
 function parseTokens(tokens) {
-  tokens.forEach((token) => {
+  tokens.forEach((token, idx, arr) => {
     if (lzAttr[token.tag]) {
+      _tmp = [];
       const addition = toArray(lzAttr[token.tag]);
+      console.log([...addition].join(""))
       token.attrSet("loading", [...addition].join(" "));
+      _tmp.push(token);
     }
+
     if (token.children) {
       parseTokens(token.children);
     }
@@ -17,6 +31,7 @@ function parseTokens(tokens) {
 
 function parseState(state) {
   parseTokens(state.tokens);
+  state.tokens = [_nsOpenToken, _tmp[0], _nsCloseToken];
 }
 
 function markdownItLazyLoading(md, _lzAttr) {
